23d06001e29096aa4825b8221bd72ab7616bbd83
Mikael Yun <mikael@yy.ydhmikael.net>
2023-05-27 04:15:06 +0900

    reponsive web design revision

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "897884a1ceb12296"; }
+/******/ 		__webpack_require__.h = function() { return "61007ff0e2ca189f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
